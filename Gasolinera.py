983e198dffb4d5fbed72ab88dbc4fd1fb5468ebe
migueliiin <91721552+migueliiin@users.noreply.github.com>
2023-04-17 17:19:43 +0200

    Surtidores y su id

--- Gasolinera.py
+++ Gasolinera.py
@@ -1,25 +1,27 @@
 import random
 
 class Gasolinera():
+    """
+    Solo voy a hacer el programa para 4 surtidores
+    N=4
+    """
     # Constructor
-    def __init__(self, surtidores):
-        self.surtidores=surtidores
+    def __init__(self):
+        self.surtidores=[]
     
     # Getters
     def getSurtidores(self):
         return self.surtidores
     
-    def SurtidorLibre(self):
-        for surtidor in self.surtidores:
-            if surtidor.getTiempo() == 0:
-                return surtidor
-        return None
+    def __str__(self):
+        return "Gasolinera con " + str(len(self.surtidores)) + " surtidores"
     
     
 
 class Surtidor():
     # Constructor
     def __init__(self):
+        self.id=round(random.random(300),0)
         self.en_uso=False
         self.tiempo=random.randrange(5,10) # En vez de ser de 5 a 10 minutos es de 5 a 10 segundos
 
