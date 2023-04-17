6a7c213b12b82bb72a80ed9c272a453e0c4c1743
migueliiin <91721552+migueliiin@users.noreply.github.com>
2023-04-17 17:03:03 +0200

    En un momentito :D

--- Coche.py
+++ Coche.py
@@ -3,8 +3,15 @@ from Gasolinera import Surtidor
 
 class Coche():
 
+    def __init__(self):
+        self.repostado = False
+
     def repostar(self, surtidor:Surtidor):
         tiempo_a_esperar = surtidor.getTiempo()
         while time.time() < tiempo_a_esperar:
             print("Repostando...")
             time.sleep(1)
+        self.repostado = True
+
+    def getRepostado(self):
+        return self.repostado
