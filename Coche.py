b33e3104543bb97bdbc30f6b38080bdf587e2dba
migueliiin <91721552+migueliiin@users.noreply.github.com>
2023-04-17 17:48:50 +0200

    new main

--- Coche.py
+++ Coche.py
@@ -20,11 +20,10 @@ class Coche():
     def getRepostado(self):
         return self.repostado
     
-if __name__=="__main__":
-    coche = Coche()
-    surtidor = Surtidor()
-    gasolinera = Gasolinera()
-    coche.repostar(surtidor)
-    print(gasolinera.str())
-    print(surtidor.getTiempo())
-    print(coche.getRepostado())
+
+if __name__=='__main__':
+    coches = []
+    for i in range(10):
+        coches.append(Coche())
+    gas = Gasolinera()
+    print(gas.cuantos_surtidores_str())
