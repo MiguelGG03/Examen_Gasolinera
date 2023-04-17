7874944b344d737bde35382e642c5a30169dd550
migueliiin <91721552+migueliiin@users.noreply.github.com>
2023-04-17 17:04:44 +0200

    Getters y Setters - Gasolinera

--- Gasolinera.py
+++ Gasolinera.py
@@ -9,14 +9,31 @@ class Gasolinera():
     def getSurtidores(self):
         return self.surtidores
     
+    def SurtidorLibre(self):
+        for surtidor in self.surtidores:
+            if surtidor.getTiempo() == 0:
+                return surtidor
+        return None
+    
     
 
 class Surtidor():
     # Constructor
     def __init__(self):
+        self.en_uso=False
         self.tiempo=random.randrange(5,10)
 
     # Getters
     def getTiempo(self):
         return self.tiempo
+    
+    def getEnUso(self):
+        return self.en_uso
+    
+    # Setters
+    def setTiempo(self, tiempo):
+        self.tiempo=tiempo
+
+    def setEnUso(self, en_uso):
+        self.en_uso=en_uso
 
