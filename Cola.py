894e50a7412b19977e804e002227d09efd1f1392
migueliiin <91721552+migueliiin@users.noreply.github.com>
2023-04-17 17:14:43 +0200

    Class cola para los surtidores

---
+++ Cola.py
@@ -0,0 +1,16 @@
+class Cola():
+
+    def __init__(self):
+        self.items = []
+
+    def estaVacia(self):
+        return self.items == []
+
+    def agregar(self, item):
+        self.items.insert(0,item)
+
+    def avanzar(self):
+        return self.items.pop(0)
+
+    def tamano(self):
+        return len(self.items)
